edcd2fad8cad21f39eba6e82c65360c8f0cf5661
Igor Costa <igorpessanhac@gmail.com>
2025-08-21 20:26:43 -0300

    Cursor Update

---
+++ src/ReplicatedStorage/RobuxItems.lua
@@ -0,0 +1,42 @@
+local itemsData = {}
+
+itemsData.Items = {
+	
+	
+	["Iron Slap"] = {
+		name = "Iron Slap",
+		description = "Heavy hand that slap players even harder than the normal one.",
+		imageId = "rbxassetid://17465852777",
+		gamepassId = 1403836912,
+	},
+	
+	["Gravity Coil"] = {
+		name = "Gravity Coil",
+		description = "Reduce your gravity, allowing you to jump higher, if needed.",
+		imageId = "rbxassetid://128850211",
+		gamepassId = 1403753041,
+	},
+	
+	["Golden Slap"] = {
+		name = "Golden Slap",
+		description = "Even heavier than the Iron Slap, find out what it does.",
+		imageId = "rbxassetid://17465852777",
+		gamepassId = 1403757030,
+	},
+	
+	["Fusion Coil"] = {
+		name = "Fusion Coil",
+		description = "Speed and Gravity combined, what is the result of this?",
+		imageId = "rbxassetid://5013021814",
+		gamepassId = 1403795030,
+	},
+	
+	["Rainbow Carpet"] = {
+		name = "Rainbow Carpet",
+		description = "Is that anything more OP than flying in a obby?",
+		imageId = "rbxassetid://4006230910",
+		gamepassId = 1403918721,
+	},
+}
+
+return itemsData
