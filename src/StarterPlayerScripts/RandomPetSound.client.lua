b87e71bb22f672f25f4bb8e978fd2ed1d20b7550
Igor Costa <igorpessanhac@gmail.com>
2025-09-02 15:51:08 -0300

    Initial start

--- src/StarterPlayerScripts/RandomPetSound.client.lua
+++
@@ -1,5 +0,0 @@
-local RunService = game:GetService("RunService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local HelperModule = require(ReplicatedStorage:WaitForChild("Helper"))
-
-HelperModule.PlayRandomPetSound()
